bd487b49bc8df2faa7939ec753128edcf797a1e6
Yura Volokitin <yuri.volokitin@gmail.com>
2025-07-28 01:12:55 +0200

    update icons and android nsk versions

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,10 @@ plugins {
 }
 
 android {
+    // "ai.annotateit"
     namespace = "com.example.vap"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,6 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
+        // "ai.annotateit"
         applicationId = "com.example.vap"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
