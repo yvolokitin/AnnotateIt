c72894582a66be9fa5a4f59b8df0d8719a9fcfc7
Yura Volokitin <yuri.volokitin@gmail.com>
2025-08-11 18:59:47 +0200

    merge decoder name change

--- assets/models_sam/models.ts
+++ assets/models_sam/models.ts
@@ -3,5 +3,5 @@
 
 export const SegmentAnythingModels = {
     encoder: new URL('./mobile_sam.encoder.onnx', import.meta.url).toString(),
-    decoder: new URL('./sam_vit_h_4b8939.decoder.onnx', import.meta.url).toString(),
+    decoder: new URL('./mobile_sam.decoder.onnx', import.meta.url).toString(),
 };
